83b59306660b74635eeb9a7aecffe407023da26a
F-TNK <fabiotanakasantos@gmail.com>
2026-06-23 13:14:13 -0300

    first commit

---
+++ src/test/java/com/senai/frotaFront/FrotaFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.frotaFront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrotaFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
